cedf6fd290410f76a112b014499b22e199d2f346
Starangelutipon <143515729+Starangelutipon@users.noreply.github.com>
2026-05-17 15:24:01 +0300

    добавление delete()
    возможность удалять книгу по названию и автору

--- BookTrack.py
+++ BookTrack.py
@@ -1,3 +1,13 @@
+books = []
+def delete():
+    name=input('название: ').strip()
+    author=input('автор: ').strip()
+    for book in books:
+        if name==book['имя'] and author==['автор']:
+            books.remove(book)
+            print('книга удалена')
+            return
+    print('ошибка в имени автора или названии')
 def load_books():
     pass
 def save_books(books):
